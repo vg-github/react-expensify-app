fc54043e4ef1323294eb1e4c298088b25f489a76
vg-github <vladimir.ghetau@gmail.com>
2018-08-15 16:20:02 +0100

    addded snapshot tests for components

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
-        <h1>Expense List</h1>
         {props.expenses.length == 0 && <p>No expenses available.</p>}
         {props.expenses.map((expense) => <ExpenseListItem key={expense.id} {...expense} />)}
     </div>
