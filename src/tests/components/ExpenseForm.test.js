a12446d6d0f0b267d52e7545ca814641596b7f67
vg-github <vladimir.ghetau@gmail.com>
2018-08-15 17:10:49 +0100

    finished tests for ExpenseForm

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -82,4 +83,18 @@ test('should call onSubmit prop for valid form submission', () => {
         note: expenses[0].note,
         createdAt: expenses[0].createdAt
     });
+});
+
+test('should set new date onDateChange', () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('should set calendar onFocusChange', () => {
+    const focused = true;
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
+    expect(wrapper.state('calendarFocused')).toBe(focused);
 });
