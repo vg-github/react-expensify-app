23d8ca88788765d3e4bd36f035b5d155d253aabd
vg-github <vladimir.ghetau@gmail.com>
2018-08-16 15:37:58 +0100

    added production build

--- src/app.js
+++ src/app.js
@@ -11,12 +11,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({description: 'WaterBill', amount: 4500 }));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 100, createdAt: 1000 }));
-store.dispatch(addExpense({description: 'Rent', amount: 109500 }));
-
-
 const state = store.getState();
 const visibleExenses = getVisibleExpenses( state.expenses, state.filters );
 
