9b01c1ee2f01251ffe7075cea55c3f9365b0d45c
vg-github <vladimir.ghetau@gmail.com>
2018-08-19 22:14:49 +0100

    fetching expenses  from firebase

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import configureStore from './stores/configureStore';
 import getVisibleExpenses from './selectors/expenses'
@@ -21,4 +21,8 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then( () => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
