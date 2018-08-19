d00b97cb4b16314002c6efa989e2657311372a4e
vg-github <vladimir.ghetau@gmail.com>
2018-08-19 22:46:12 +0100

    added remove expense for firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -41,6 +41,14 @@ export const removeExpense = (
         id
 });
 
+export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({id}));
+        });
+    };
+};
+
 // EDIT_EXPENSE
 export const editExpense = ( id, updates) => ({
     type: 'EDIT_EXPENSE',
